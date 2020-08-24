679d78bdbb78502aefd99571933a795035f8ba8e
rawkingo <69311715+rawkingo@users.noreply.github.com>
2020-08-24 17:29:28 +0300

    C# GCD

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+ 
+public class Program
+{
+    static int GCD(int num1, int num2)
+    {
+        int a;
+ 
+        while (num2 != 0)
+        {
+            a = num1 % num2;
+            num1 = num2;
+            num2 = a;
+        }
+ 
+        return num1;
+    }
+ 
+    static int Main(string[] args)
+    {
+        int x, y;
+ 
+        Console.Write("Try the first number :) : ");
+        x = int.Parse(Console.ReadLine());
+ 
+        Console.Write("And the second one?: ");
+        y = int.Parse(Console.ReadLine());
+ 
+        Console.Write("\nGCD OF ");
+ 
+        Console.WriteLine("{0} and {1} is {2}", x, y, GCD(x, y));
+ 
+        
+        return 0;
+    }
+}
